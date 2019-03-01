504e1cc6f0b96bc707bed6acce05b16b80d98bf2
Eric Baril <baril27@gmail.com>
2019-03-01 10:51:04 -0800

    Initial commit

---
+++ src/extensions/paramFilter/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IParamFilterApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'ParamFilterApplicationCustomizerStrings' {
+  const strings: IParamFilterApplicationCustomizerStrings;
+  export = strings;
+}
